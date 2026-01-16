e9ad82fb30adfa1ed0beb80f7243d0d65f42b09b
狐狸的狐狸画 <740740048@qq.com>
2026-01-17 03:37:59 +0800

    一点小测试

--- apps.js
+++ apps.js
@@ -9,6 +9,7 @@
     { name: "今岭笔记", path: "jlbj/index.html" },
     { name: "今岭流水可视化", path: "jlksh/index.html" },
     { name: "今岭快驴可视化", path: "jlklksh/index.html" },
+    { name: "今岭快驴数据", path: "jlklsj/index.html" },
     { name: "PPT", path: "ppt/index.html" },
     { name: "阅图", path: "vista/index.html" },
     { name: "SVG", path: "svg/index.html" },
