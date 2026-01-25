b66829c42c3f0df787a3750ae1218e484e11dd2e
狐狸的狐狸画 <740740048@qq.com>
2026-01-26 03:11:14 +0800

    Refactor: 整合模块至单页面并优化UI设计，添加脑图功能

--- apps.js
+++ apps.js
@@ -3,7 +3,7 @@
     { name: "日历", path: "calendar/index.html" },
     { name: "待办事项", path: "todo/index.html" },
     { name: "测试", path: "test/index.html" },
-    { name: "灵感词云", path: "wordcanvas/index.html" },
+    { name: "思维导图", path: "mindmap/index.html" },
     { name: "云盘", path: "cloud/index.html" },
     { name: "吃什么", path: "eatwhat/index.html" },
     { name: "今岭笔记", path: "jlbj/index.html" },
