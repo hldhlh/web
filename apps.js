789e006e420aca6e0929db8638ed9e33a0a6af93
狐狸的狐狸画 <31961068+hldhlh@users.noreply.github.com>
2026-05-15 17:56:30 +0800

    重构

--- apps.js
+++ apps.js
@@ -3,7 +3,7 @@ export async function getAppDirectories() {
     { name: "节假日日历", path: "calendar/index.html" },
     { name: "待办事项", path: "todo/index.html" },
     { name: "图片转换", path: "image-converter/index.html" },
-    { name: "思维矩阵", path: "wordcanvas/index.html" },
+    { name: "思维矩阵", path: "thought-matrix/index.html" },
     { name: "云盘", path: "cloud/index.html" },
     { name: "吃什么", path: "eatwhat/index.html" },
     { name: "今岭笔记", path: "jlbj/index.html" },
